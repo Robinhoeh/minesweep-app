5a4f0b2fdae649112ed32c1c261359a1386b5bee
Robin Watson <drumsrobin@gmail.com>
2017-10-11 08:19:16 -0400

    update js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import './index.css';
+const printBoard= (board) => {		//these are placed inside each item
+	console.log('Current board: ');
+	console.log(board[0].join(' | ') );
+	console.log(board[1].join(' | ') );
+	console.log(board[2].join(' | ') );
 
-document.getElementById('root').innerHTML = 'Hello World';
+};
 
-alert('Hello World!');
+
+let board = [ //this is the blank board
+
+	[' ', ' ', ' '], 
+	[' ', ' ', ' '], 
+	[' ', ' ', ' '] 
+
+];
+
+console.log( printBoard (board) );
+
+
+// console.log(board);	//dont need to see empty array with .join() now
+
+board[0][1] = '1'; 
+board[2][2] = 'B';
+
+console.log( printBoard(board) );
+
+
+
+
